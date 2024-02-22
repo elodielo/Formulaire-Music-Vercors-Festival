0496f969a10a30309f64728545ccb798be2e9e44
Elodie <elodie.loeb.simplon@gmail.com>
2024-02-22 14:01:33 +0100

    avec classe reservations et calcul pour le prix total. Erreurs a regler avec le nbr de tentes et de camions

--- class/Reservations.php
+++ class/Reservations.php
@@ -2,11 +2,86 @@
 
 class Reservation{
     private $nbrReservation;
-    private $tarif; // choix normal et réduit
+    private $tarif; // reduit ou pas 
+    // private $nbrJours;
+    private $joursChoisis;
     private $nbrTentes;
     private $nbrCamions;
+    private $nbrEnfants;
     private $nbrCasques;
     private $nbrLuges;
+    
+    function __construct($nbrReservation, $tarif ,$joursChoisis, $nbrTentes, $nbrCamions, $nbrEnfants, $nbrCasques, $nbrLuges){
+        $this->nbrReservation = $nbrReservation;
+        $this->tarif = $tarif;
+        // $nbrJours = $this->nbrJours;
+        $this->joursChoisis = $joursChoisis;
+        $this->nbrTentes = $nbrTentes;
+        $this->nbrCamions = $nbrCamions;
+        $this->nbrEnfants = $nbrEnfants;
+        $this->nbrCasques = $nbrCasques;
+        $this->nbrLuges = $nbrCasques;
+    }
+    
+    function getNbrReservation(){
+        return $this->nbrReservation;
+    }    
+    function setNbrReservation($nbrReservation){
+        $this->nbrReservation = $nbrReservation;
+    }
+    function getTarif(){
+        return $this->tarif;
+    }    
+    function setTarif($tarif){
+        $this->tarif = $tarif;
+    }
+    // function getnbrJours(){
+    //     return $this->nbrJours;
+    // }    
+    // function setnbrJours($nbrJours){
+    //     $this->nbrJours = $nbrJours;
+    // }
+    function getjoursChoisis(){
+        return $this->joursChoisis;
+    }    
+    function setjoursChoisis($joursChoisis){
+        $this->joursChoisis = $joursChoisis;
+    }
+    function getnbrTentes(){
+        return $this->nbrTentes;
+    }    
+    function setnbrTentes($nbrTentes){
+        $this->nbrTentes = $nbrTentes;
+    }
+    function getnbrCamions(){
+        return $this->nbrCamions;
+    }    
+    function setnbrCamions($nbrCamions){
+        $this->nbrCamions = $nbrCamions;
+    }
+    function getnbrEnfants(){
+        return $this->nbrEnfants;
+    }    
+    function setnbrEnfants($nbrEnfants){
+        $this->nbrEnfants = $nbrEnfants;
+    }
+    function getnbrCasques(){
+        return $this->nbrCasques;
+    }    
+    function setnbrCasques($nbrCasques){
+        $this->nbrCasques = $nbrCasques;
+    }
+    function getnbrLuges(){
+        return $this->nbrLuges;
+    }    
+    function setnbrLuges($nbrLuges){
+        $this->nbrLuges = $nbrLuges;
+    }
+
+    function calculPrixFestival(){
+        $PrixTotal = ($this->nbrReservation*$this->tarif + $this->nbrTentes+ $this->nbrCamions+ $this->nbrCasques);
+        echo($PrixTotal);
+    }
     
     
 }
--- traitement.php
+++ traitement.php
@@ -1,6 +1,7 @@
 <?php 
 
 require './class/Client.php';
+require './class/Reservations.php';
 // require './class/dataBase.php';
 
 var_dump($_POST);
@@ -10,26 +11,95 @@ if (isset($_POST['nombrePlaces'])
 && isset($_POST['prenom']) 
 && isset($_POST['email']) 
 && isset($_POST['telephone']) 
-&& isset($_POST['email'])){
+&& isset($_POST['adressePostale'])){
+  var_dump('hello');
     $prenom = htmlspecialchars($_POST['prenom']);
     $nom = htmlspecialchars($_POST['nom']);
-    $nombrePlacesEcrites = $_POST['nombrePlaces'];
-    $nombrePlaces = (int)$nombrePlacesEcrites;
+    $nbrReservation = (int)$_POST['nombrePlaces'];
+    // $nombrePlaces = (int)$nombrePlacesEcrites;
     $telephone = $_POST['telephone'];
-    $adresse = $_POST['adresse'];
+    $adresse = $_POST['adressePostale'];
+    $tarif= 0;
+    $nbrTentes=0;
+    $nbrCamions= 0;
+    $nbrCasques = (int)$_POST['nombreCasquesEnfants'];
+    $nbrLuges = $_POST['NombreLugesEte'];
+    $joursChoisis ="";
+    $nbrEnfants= "non";
+
+    // if (filter_var($_POST['telephone'], FILTER_VALIDATE_INT)) {
+    //   $email = htmlspecialchars($_POST['telephone']);
+    // }else {
+    //   header('location:../index.php?erreur=');
+    // }
 
     if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-        $mail = htmlspecialchars($_POST['email']);
+        $email = htmlspecialchars($_POST['email']);
       }else {
         header('location:../index.php?erreur=');
       }
 
-    if($_POST['tarifReduit']){
-      $tarif = $_POST['tarifReduit'];
+
+    
+    if(isset($_POST['pass1jour'])){
+      $tarif = 40;
+    } elseif (isset($_POST['pass2jours'])) {
+      $tarif = 70;
+    }elseif (isset($_POST['pass3jours'])) {
+      $tarif = 100;
     }
-      
-      $client = new Client($nom, $prenom, $email, $telephone, $adresse); 
-      
-      // $SaveClient = $dataBase->enregistrerClient($client);
-      var_dump($client);
+    if(isset($_POST['pass1jourreduit'])){
+      $tarif = 25;
+    } elseif (isset($_POST['pass2joursreduit'])) {
+      $tarif = 50;
+    }elseif (isset($_POST['pass3joursreduit'])) {
+      $tarif = 65;
     }
+
+    if(isset($_POST['choixJour1'])){
+      $joursChoisis = "01/07";
+    }elseif (isset($_POST['choixJour2'])){
+      $joursChoisis = "02/07";
+    }elseif (isset($_POST['choixJour3'])){
+      $joursChoisis = "03/07";
+    }elseif (isset($_POST['choixJour12'])){
+      $joursChoisis = "du 01/07 au 02/07";
+    }elseif (isset($_POST['choixJour23'])){
+      $joursChoisis = "du 02/07 au 03/07";
+    }
+
+    if(isset($_POST['tenteNuit1'])){
+      $nbrTentes += 5;
+    } elseif (isset($_POST['tenteNuit2'])) {
+      $nbrTentes += 5;
+    }elseif (isset($_POST['tenteNuit3'])) {
+      $nbrTentes += 5;
+    }elseif (isset($_POST['tente3Nuits'])) {
+      $nbrTentes += 12;
+    }
+
+    if(isset($_POST['vanNuit1'])){
+      $nbrCamions = +5;
+    } elseif (isset($_POST['vanNuit2'])) {
+      $nbrCamions += 5;
+    }elseif (isset($_POST['vanNuit3'])) {
+      $nbrCamions += 5;
+    }elseif (isset($_POST['van3Nuits'])) {
+      $nbrCamions += 12;
+    }
+
+    if(isset($_POST['enfantsOui'])){
+      $nbrEnfants = "oui";
+    }elseif (isset($_POST['enfantsNon'])) {
+      $nbrEnfants = "non";
+    }
+
+    $reservation = new Reservation($nbrReservation, $tarif, $joursChoisis,$nbrTentes, $nbrCamions, $nbrEnfants, $nbrCasques, $nbrLuges);
+    // $client = new Client($nom, $prenom, $email, $telephone, $adresse); 
+    $reservation->calculPrixFestival();
+    var_dump($tarif);
+    // var_dump($client);
+    var_dump($reservation);
+            // $SaveClient = $dataBase->enregistrerClient($client);
+      // var_dump($client);
+  }

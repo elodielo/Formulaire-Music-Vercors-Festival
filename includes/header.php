3b51e9fb74809408ee53c66650115f549ef3c2a4
Elodie <elodie.loeb.simplon@gmail.com>
2024-02-22 14:16:03 +0100

    class reservations

--- includes/header.php
+++ includes/header.php
@@ -13,10 +13,6 @@
     <div id="enTete" class="flexLigne" > 
       <img id="logo" src="./Images/logo.png" alt="logo montagne"> 
       <h1> Music Vercors Festival </h1>
-    
-      
-    
-    
     </div>
 
   </header>
